52070fad8e49501f4e2e693b5a77602076cf10e1
Casper Olsen <gemic@live.dk>
2025-08-04 21:51:31 +0200

    feat: export .dotnet folder

--- zsh-linux/.config/zsh/env.zsh
+++ zsh-linux/.config/zsh/env.zsh
@@ -3,6 +3,10 @@ export ZSH="$HOME/.oh-my-zsh"
 
 export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 
+# dotnet
+export DOTNET_ROOT=$HOME/.dotnet
+export PATH=$HOME/.dotnet:$PATH
+
 # vcpkg
 export PATH=$PATH:$HOME/vcpkg
 export VCPKG_ROOT=$HOME/vcpkg
