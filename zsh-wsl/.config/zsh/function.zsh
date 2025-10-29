7dc9beb02d3e119fde300b045e5af80245dd3647
Casper Olsen <cabol@energinet.dk>
2025-10-29 15:44:05 +0100

    feat: Added gitprune function

--- zsh-wsl/.config/zsh/function.zsh
+++ zsh-wsl/.config/zsh/function.zsh
@@ -1,7 +1,14 @@
 # Commands
+
+# Count the commits on a branch that have been made since the branch was created
 gitcc() {
   local remote="${1:-origin}"
   local branch
   branch=$(git remote show "$remote" 2>/dev/null | sed -n '/HEAD branch/s/.*: //p')
   git rev-list --count HEAD ^"$remote"/"$branch" 2>/dev/null
+}
+
+# Automatically delete local Git branches whose remote tracking branches have been deleted
+gitprune() {
+  git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done
 }
