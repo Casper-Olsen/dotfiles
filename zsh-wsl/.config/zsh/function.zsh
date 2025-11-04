34158ba4c762645a70863cfb43812215a17f6530
Casper Olsen <cabol@energinet.dk>
2025-11-04 11:47:22 +0100

    feat: gitnew command

--- zsh-wsl/.config/zsh/function.zsh
+++ zsh-wsl/.config/zsh/function.zsh
@@ -12,3 +12,8 @@ gitcc() {
 gitprune() {
   git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done
 }
+
+# Automatically delete local Git branches whose remote tracking branches have been deleted
+gitnew() {
+  git switch -c "$1"
+}
