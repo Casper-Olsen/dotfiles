4e4d56baa28f5c9bae4c45e0889acd58df146b6a
Casper Olsen <cabol@energinet.dk>
2025-12-10 09:44:32 +0100

    feat: add git remote prune origin to gitprune

--- zsh-wsl/.config/zsh/function.zsh
+++ zsh-wsl/.config/zsh/function.zsh
@@ -10,6 +10,7 @@ gitcc() {
 
 # Automatically delete local Git branches whose remote tracking branches have been deleted
 gitprune() {
+  git remote prune origin
   git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done
 }
 
