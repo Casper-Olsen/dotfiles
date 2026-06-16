e65b58ac0eacd2fb4281a6cfa51f3cdb0514ab72
Casper Olsen <gemic@live.dk>
2026-06-16 23:29:11 +0200

    chore: Remove alias and comment about 0.12

--- zsh-linux/.config/zsh/aliases.zsh
+++ zsh-linux/.config/zsh/aliases.zsh
@@ -1,3 +1,10 @@
 # Set personal aliases, overriding those provided by Oh My Zsh libs, plugins, and themes.
 alias vim="nvim"
-alias nvim11="~/bin/nvim-0.11/nvim-linux-x86_64.appimage"
+
+# When Fedora ships Neovim 0.12, switch back to the packaged version:
+#   rm ~/.local/bin/nvim
+#   hash -r
+#   sudo dnf upgrade neovim
+#   nvim --version
+# If that shows Fedora's 0.12, the AppImage install can be removed:
+#   rm -r ~/.local/opt/nvim/0.12
